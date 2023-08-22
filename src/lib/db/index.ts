2292e77c76476c97055e0dbe369b17cc684de8fe
harshal <harshalmahapure44@gmail.com>
2023-08-22 18:42:06 +0300

    ok

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,17 +1,12 @@
-import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
- 
+import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
-// for query purposes   
-
-
-
-const queryClient = postgres(process.env.DB_URL,{
-    max:1,
-    ssl: "require",
-}); 
+const queryClient = postgres(process.env.DB_URL, {
+  max: 1,
+  ssl: "require",
+});
 
 const db: PostgresJsDatabase = drizzle(queryClient);
 
- 
+// database object
 export default db;
